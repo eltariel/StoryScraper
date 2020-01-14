ebd63c096297587679eda2b98690fcb5f2588e0e
Ellie <ellie@eltariel.com>
2020-01-14 22:50:16 +1100

    Super early start at web frontend

---
+++ StoryScraper.Web/Controllers/StoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoryScraper.Web.Models;
+
+namespace StoryScraper.Web.Controllers
+{
+    public class StoryController : Controller
+    {
+        // GET
+        public async Task<IActionResult> Details(string storyUrl)
+        {
+            var url = new Uri(storyUrl);
+            var site = SiteFactory.GetSiteFor(url);
+            var story = await site.GetStory(url);
+            
+            var model = new StoryViewModel(url, story);
+            return View(model);
+        }
+    }
+}
---
+++ StoryScraper.Web/Models/StoryViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoryScraper.Web.Models
+{
+    public class StoryViewModel
+    {
+        private readonly Story story;
+
+        public StoryViewModel(Uri storyUrl, Story story)
+        {
+            this.story = story;
+            StoryUrl = storyUrl;
+            Categories = story.Categories.Select(c => new CategoryViewModel(c)).ToList();
+        }
+
+        public Uri StoryUrl { get; }
+
+        public string Title => story.Title;
+
+        public string BaseUrl => story.BaseUrl;
+        
+        public List<CategoryViewModel> Categories { get; }
+    }
+
+    public class CategoryViewModel
+    {
+        private readonly Category category;
+
+        public CategoryViewModel(Category category)
+        {
+            this.category = category;
+        }
+
+        public string Id => category.Id;
+
+        public string Name => category.Name;
+    }
+}
